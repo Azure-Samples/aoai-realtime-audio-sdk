38258ec050b522fb004a3c081d0c083adbbc9aa7
Gerardo Lecaros <10088504+glecaros@users.noreply.github.com>
2024-10-14 16:51:47 +0000

    Update download scripts to point to 0.5.0 + formatting fixes.

--- python/samples/download-wheel.sh
+++ python/samples/download-wheel.sh
@@ -3,7 +3,7 @@
 OWNER="Azure-Samples"
 REPO="aoai-realtime-audio-sdk"
 FILTER="-py3-none-any.whl"
-RELEASE="py/v0.4.4"
+RELEASE="py/v0.5.0"
 OUTPUT_DIR="."
 
 API_URL="https://api.github.com/repos/$OWNER/$REPO/releases/tags/$RELEASE"

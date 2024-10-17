0ccfe8ac46e958df2161f76f300cba1bf24e0fd8
Gerardo Lecaros <10088504+glecaros@users.noreply.github.com>
2024-10-17 04:59:46 +0000

    wip

--- javascript/standalone/src/client.ts
+++ javascript/standalone/src/client.ts
@@ -26,6 +26,7 @@ import {
   azureOpenAISettings,
   openAISettings,
 } from "./util/connection-settings";
+import { MessageQueue } from "./util/message_queue";
 
 export class LowLevelRTClient {
   public requestId: string | undefined;
@@ -71,8 +72,8 @@ export class LowLevelRTClient {
     options: RTAzureOpenAIOptions,
   );
   constructor(
-    uriOrCredential: URL | KeyCredential | TokenCredential,
-    credentialOrOptions?: KeyCredential | TokenCredential | RTOpenAIOptions,
+    uriOrCredential: URL | KeyCredential,
+    credentialOrOptions: KeyCredential | TokenCredential | RTOpenAIOptions,
     options?: RTAzureOpenAIOptions,
   ) {
     const settings = (() => {
@@ -114,311 +115,39 @@ export class LowLevelRTClient {
     await this.client.close();
   }
 }
-/*
-export interface RTMessageContentChunk {
-  type: MessageContentType;
-  data: string;
-  index: number;
-}
-
-class RTMessage implements AsyncIterable<RTMessageContentChunk> {
-  private constructor(
-    readonly id: string,
-    readonly previousId: string | undefined,
-    readonly conversationLabel: string,
-    readonly content: MessageContent[],
-    private readonly receive: () => Promise<ServerMessageType>,
-  ) {}
-
-  async *[Symbol.asyncIterator](): AsyncIterator<RTMessageContentChunk> {
-    while (true) {
-      const serverMessage = await this.receive();
-      if (serverMessage === null) {
-        break;
-      }
-      if (serverMessage.event === "message_added") {
-        break;
-      }
-      if (
-        serverMessage.event === "add_content" &&
-        serverMessage.message_id === this.id
-      ) {
-        // const content = this.content[serverMessage.index];
-
-        // switch (serverMessage.type) {
-        //   case "text":
-        //     if (content.type !== "text") {
-        //       throw new Error("Unexpected content type");
-        //     }
-        //     content.text += serverMessage.data;
-        //     break;
-        //   case "audio":
-        //     if (content.type !== "audio") {
-        //       throw new Error("Unexpected content type");
-        //     }
-        //     content.audio += serverMessage.data;
-        //     break;
-        //   case "tool_call":
-        //     if (content.type !== "tool_call") {
-        //       throw new Error("Unexpected content type");
-        //     }
-        //     content.arguments += serverMessage.data;
-        //     break;
-        //   default:
-        //     break;
-        // }
-        yield {
-          type: serverMessage.type,
-          data: serverMessage.data,
-          index: serverMessage.index,
-        };
-      }
-    }
-  }
-
-  static _create(
-    id: string,
-    previousId: string | undefined,
-    conversationLabel: string,
-    content: MessageContent[],
-    receive: () => Promise<ServerMessageType>,
-  ): RTMessage {
-    return new RTMessage(id, previousId, conversationLabel, content, receive);
-  }
-}
-
-export type { RTMessage };
-
-export interface RTConversationConfiguration {
-  system_message?: string;
-  voice?: Voice;
-  subscribe_to_user_audio?: boolean;
-  output_audio_format?: AudioFormat;
-  tools?: unknown;
-  tool_choice?: ToolChoice;
-  temperature?: number;
-  max_tokens?: number;
-  disable_audio?: boolean;
-}
-
-class RTConversation implements AsyncIterable<RTMessage> {
-  private allMessagesQueue: MessageQueue<ServerMessageType>;
-  private messageQueue: MessageQueue<ServerMessageType>;
-
-  private constructor(
-    readonly label: string,
-    private readonly client: LowLevelRTClient,
-    receive: () => Promise<ServerMessageType | null>,
-  ) {
-    this.allMessagesQueue = new MessageQueue<ServerMessageType>(
-      receive,
-      (message) => {
-        switch (message.event) {
-          case "add_message":
-            return "CONVERSATION-MESSAGE";
-          case "add_content":
-            return "MESSAGE";
-          case "message_added":
-            return "MESSAGE";
-          case "generation_finished":
-            return "CONVERSATION-CONTROL";
-          default:
-            break;
-        }
-        return null;
-      },
-    );
-    this.messageQueue = new MessageQueue<ServerMessageType>(
-      async () => {
-        return await this.allMessagesQueue.receive("MESSAGE");
-      },
-      (message) => {
-        switch (message.event) {
-          case "add_content":
-            return message.message_id;
-          case "message_added":
-            return message.id;
-          default:
-            break;
-        }
-        return null;
-      },
-    );
-  }
-
-  async configure(config: RTConversationConfiguration): Promise<void> {
-    await this.client.send({ event: "update_conversation_config", ...config });
-  }
-
-  async *[Symbol.asyncIterator](): AsyncIterator<RTMessage> {
-    while (true) {
-      const serverMessage = await this.allMessagesQueue.receive(
-        "CONVERSATION-MESSAGE",
-      );
-      if (serverMessage === null) {
-        break;
-      }
-      if (serverMessage.event !== "add_message") {
-        throw new Error("Unexpected message type");
-      }
-      const message = RTMessage._create(
-        serverMessage.message.id,
-        serverMessage.previous_id,
-        serverMessage.conversation_label,
-        serverMessage.message.content,
-        async () => {
-          return await this.messageQueue.receive(serverMessage.message.id);
-        },
-      );
-      yield message;
-    }
-  }
-
-  async *controlMessages(): AsyncIterable<ServerGenerationFinishedMessage> {
-    while (true) {
-      const message = await this.allMessagesQueue.receive(
-        "CONVERSATION-CONTROL",
-      );
-      if (message === null) {
-        break;
-      }
-      if (message.event !== "generation_finished") {
-        throw new Error("Unexpected message type");
-      }
-      yield message;
-    }
-  }
-
-  static _create(
-    label: string,
-    client: LowLevelRTClient,
-    receive: () => Promise<ServerMessageType | null>,
-  ) {
-    return new RTConversation(label, client, receive);
-  }
-}
-
-export type { RTConversation };
-
-export interface RTSessionConfiguration {
-  turn_detection?: TurnDetection;
-  input_audio_format?: AudioFormat;
-  transcribe_input?: boolean;
-  vad?: VADConfiguration;
-}
 
 export class RTClient {
   private client: LowLevelRTClient;
   private messageQueue: MessageQueue<ServerMessageType>;
-  private conversationQueue: MessageQueue<ServerMessageType>;
-  private conversationMap: Map<string, string> = new Map();
 
-  constructor(key: string);
-  constructor(uri: URL, key: string);
-  constructor(uriOrKey: string | URL, key?: string) {
-    this.client =
-      key === undefined
-        ? new LowLevelRTClient(uriOrKey as string)
-        : new LowLevelRTClient(uriOrKey as URL, key);
-    this.messageQueue = new MessageQueue<ServerMessageType>(
-      async () => {
-        for await (const message of this.client.messages()) {
-          return message;
-        }
-        return null;
-      },
-      (message) => {
-        switch (message.event) {
-          case "start_session":
-          case "error":
-          case "vad_speech_started":
-          case "vad_speech_stopped":
-          case "input_transcribed":
-          case "generation_canceled":
-          case "send_state":
-            return "SESSION";
-          case "add_message":
-          case "add_content":
-          case "message_added":
-          case "generation_finished":
-            return "CONVERSATION";
-          default:
-            break;
-        }
-        return null;
-      },
-    );
-    this.conversationQueue = new MessageQueue<ServerMessageType>(
-      async () => {
-        return await this.messageQueue.receive("CONVERSATION");
-      },
-      (message) => {
-        switch (message.event) {
-          case "add_message":
-            this.conversationMap.set(
-              message.message.id,
-              message.conversation_label,
-            );
-            return message.conversation_label;
-          case "add_content":
-            return this.conversationMap.get(message.message_id) ?? null;
-          case "message_added":
-            this.conversationMap.delete(message.id);
-            return message.conversation_label;
-          case "generation_finished":
-            return message.conversation_label;
-          default:
-            break;
-        }
-        return null;
-      },
-    );
-  }
-
-  async configure(config: RTSessionConfiguration): Promise<void> {
-    await this.client.send({ event: "update_session_config", ...config });
-  }
-
-  getDefaultConversation(): RTConversation {
-    return RTConversation._create("default", this.client, async () => {
-      return this.conversationQueue.receive("default");
-    });
-  }
-
-  async createConversation(label: string): Promise<RTConversation> {
-    if (label === "default") {
-      throw new Error("Cannot create conversation with label 'default'");
-    }
-    await this.client.send({ event: "create_conversation", label });
-    return RTConversation._create(label, this.client, async () => {
-      return this.conversationQueue.receive(label);
-    });
-  }
-
-  async deleteConversation(label: string): Promise<void> {
-    if (label === "default") {
-      throw new Error("Cannot delete conversation with label 'default'");
-    }
-    await this.client.send({ event: "delete_conversation", label });
-  }
-
-  async sendAudio(audio: ArrayBuffer): Promise<void> {
-    const base64Encoded = Buffer.from(audio).toString("base64");
-    await this.client.send({ event: "add_user_audio", data: base64Encoded });
-  }
-
-  async *controlMessages(): AsyncIterable<ServerMessageType> {
-    while (true) {
-      const message = await this.messageQueue.receive("SESSION");
-      if (message === null) {
-        break;
+  constructor(credential: KeyCredential, options: RTOpenAIOptions);
+  constructor(
+    uri: URL,
+    credential: KeyCredential | TokenCredential,
+    options: RTAzureOpenAIOptions,
+  );
+  constructor(
+    uriOrCredential: URL | KeyCredential,
+    credentialOrOptions: KeyCredential | TokenCredential | RTOpenAIOptions,
+    options?: RTAzureOpenAIOptions,
+  ) {
+    this.client = (() => {
+      if (isKeyCredential(uriOrCredential)) {
+        return new LowLevelRTClient(
+          uriOrCredential,
+          credentialOrOptions as RTOpenAIOptions,
+        );
+      } else {
+        return new LowLevelRTClient(
+          uriOrCredential as URL,
+          credentialOrOptions as KeyCredential | TokenCredential,
+          options as RTAzureOpenAIOptions,
+        );
       }
-      yield message;
-    }
+    })();
   }
 
-  async close(): Promise<void> {
-    await this.client.close();
+  get requestId(): string | undefined {
+    return this.client.requestId;
   }
 }
-*/
--- javascript/standalone/src/util/message_queue.ts
+++ javascript/standalone/src/util/message_queue.ts
@@ -2,109 +2,174 @@
 // Licensed under the MIT License.
 
 type ReceiveFunction<T> = () => Promise<T | null>;
-type IdExtractor<T> = (message: T) => string | null;
 
 type ResolveFn<T> = (value: T | null) => void;
 type RejectFn<E> = (reason: E) => void;
 
+type Predicate<T> = (message: T) => boolean;
+
 export class MessageQueue<T> {
   private messages: T[] = [];
-  private waitingReceivers: Map<string, [ResolveFn<T>, RejectFn<Error>][]> =
-    new Map();
+  protected waitingReceivers: [
+    Predicate<T>,
+    [ResolveFn<T>, RejectFn<Error>],
+    AbortController
+  ][] = [];
   private isPolling: boolean = false;
+  private pollPromise: Promise<void> | null = null;
 
-  constructor(
-    private receiveDelegate: ReceiveFunction<T>,
-    private readonly idExtractor: IdExtractor<T>,
-  ) {}
+  constructor(private receiveDelegate: ReceiveFunction<T>) {}
+
+  private pushBack(message: T) {
+    this.messages.push(message);
+  }
+
+  private findAndRemove(predicate: Predicate<T>): T | null {
+    const index = this.messages.findIndex(predicate);
+    if (index === -1) {
+      return null;
+    }
+    return this.messages.splice(index, 1)[0];
+  }
 
   private async pollReceive(): Promise<void> {
+    console.log("pollReceive");
     if (this.isPolling) {
-      return;
+      console.log("pollReceive: already polling");
+      return this.pollPromise;
     }
 
+    this.isPolling = true;
+    this.pollPromise = this.doPollReceive();
+    return this.pollPromise;
+  }
+
+  private async doPollReceive(): Promise<void> {
     try {
-      this.isPolling = true;
       while (this.isPolling) {
+        console.log("pollReceive: polling");
         const message = await this.receiveDelegate();
-        if (!message) {
+        console.log("pollReceive: received message", message);
+        if (message === null) {
+          console.log("pollReceive: end of stream");
           this.notifyEndOfStream();
           break;
         }
         this.notifyReceiver(message);
+        if (this.waitingReceivers.length === 0 && this.messages.length === 0) {
+          console.log("pollReceive: no more waiting receivers or queued messages");
+          break;
+        }
       }
     } catch (error) {
+      console.log("pollReceive: error", error);
       this.notifyError(error);
     } finally {
+      console.log("pollReceive: finally");
       this.isPolling = false;
+      this.pollPromise = null;
     }
   }
 
   private notifyError(error: Error): void {
-    for (const [, receivers] of this.waitingReceivers) {
-      for (const [_, reject] of receivers) {
-        reject(error);
-      }
+    while (this.waitingReceivers.length > 0) {
+      const [_predicate, [_resolve, reject], _controller] = this.waitingReceivers.shift()!;
+      reject(error);
     }
-    this.waitingReceivers.clear();
   }
 
   private notifyEndOfStream(): void {
-    for (const [, receivers] of this.waitingReceivers) {
-      for (const [resolve, _] of receivers) {
-        resolve(null);
-      }
+    while (this.waitingReceivers.length > 0) {
+      const [_predicate, [resolve, _reject], _controller] = this.waitingReceivers.shift()!;
+      resolve(null);
     }
-    this.waitingReceivers.clear();
   }
 
   private notifyReceiver(message: T): void {
-    const id = this.idExtractor(message);
-    if (id === null) {
-      /* If the extractor return null, the message is not relevant for this queue */
-      return;
-    }
-    if (!this.waitingReceivers.has(id)) {
-      /* If no listeners are waiting for this id, queue it for later consumption */
-      this.messages.push(message);
-      return;
-    }
-
-    const [resolve, _] = this.waitingReceivers.get(id)!.shift()!;
-    if (this.waitingReceivers.get(id)?.length === 0) {
-      this.waitingReceivers.delete(id);
-    }
-    resolve!(message);
-
-    if (this.getAllWaitingReceiversCount() === 0) {
-      this.isPolling = false;
-    }
-  }
-
-  private getAllWaitingReceiversCount(): number {
-    return Array.from(this.waitingReceivers.values()).reduce(
-      (sum, receivers) => sum + receivers.length,
-      0,
+    const index = this.waitingReceivers.findIndex(
+      ([predicate, [_resolve, _reject], _controller]) => predicate(message),
     );
+    console.log("notifyReceiver", index, message);
+    if (index === -1) {
+      this.pushBack(message);
+      console.log("notifyReceiver: pushed back message", message);
+      return;
+    }
+
+    const [_predicate, [resolve, _reject], _controller] = this.waitingReceivers.splice(
+      index,
+      1,
+    )[0];
+    resolve(message);
   }
 
-  async receive(receiverId: string): Promise<T | null> {
-    return new Promise<T | null>((resolve, reject) => {
-      const foundIndex = this.messages.findIndex((message) => {
-        this.idExtractor(message) == receiverId;
-      });
-      if (foundIndex >= 0) {
-        const foundMessage = this.messages.splice(foundIndex, 1)[0];
-        return resolve(foundMessage);
-      }
-      if (!this.waitingReceivers.has(receiverId)) {
-        this.waitingReceivers.set(receiverId, []);
-      }
-      this.waitingReceivers.get(receiverId)!.push([resolve, reject]);
+  queuedMessageCount(): number {
+    return this.messages.length;
+  }
 
-      if (!this.isPolling) {
-        this.pollReceive();
-      }
+  receive(predicate: (message: T) => boolean): { promise: Promise<T | null>; cancel: () => void } {
+    const foundMessage = this.findAndRemove(predicate);
+    if (foundMessage !== null) {
+      return {
+        promise: Promise.resolve(foundMessage),
+        cancel: () => {} // No-op for immediately resolved promises
+      };
+    }
+
+    const controller = new AbortController();
+    let resolvePromise: ResolveFn<T>, rejectPromise: RejectFn<Error>;
+
+    const promise = new Promise<T | null>((resolve, reject) => {
+      resolvePromise = resolve;
+      rejectPromise = reject;
+
+      this.waitingReceivers.push([predicate, [resolve, reject], controller]);
+      this.pollReceive().catch(reject);
+
+      controller.signal.addEventListener('abort', () => {
+        const index = this.waitingReceivers.findIndex(
+          ([_pred, [_res, _rej], ctrl]) => ctrl === controller
+        );
+        if (index !== -1) {
+          this.waitingReceivers.splice(index, 1);
+          reject(new Error('Receive operation cancelled'));
+        }
+      });
     });
+
+    return {
+      promise,
+      cancel: () => controller.abort()
+    };
+  }
+}
+export class MessageQueueWithError<T> extends MessageQueue<T> {
+  private error?: T = undefined;
+
+  constructor(
+    receiveDelegate: ReceiveFunction<T>,
+    private errorPredicate: (message: T) => boolean,
+  ) {
+    super(receiveDelegate);
+  }
+
+  private notifyErrorMessage(message: T): void {
+    while (this.waitingReceivers.length > 0) {
+      const [_, [resolve, _reject]] = this.waitingReceivers.shift()!;
+      resolve(message);
+    }
+  }
+
+  async receive(predicate: (message: T) => boolean): Promise<T | null> {
+    if (this.error !== undefined) {
+      return this.error;
+    }
+    const message = await super.receive((message) => predicate(message) || this.errorPredicate(message));
+    if (message !== null && this.errorPredicate(message)) {
+      this.error = message;
+      this.notifyErrorMessage(message);
+      return message;
+    }
+    return message;
   }
 }
